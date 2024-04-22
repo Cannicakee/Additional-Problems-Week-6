99abb1968732db50edbc8c1409a9c47bc7f8fb2c
Cannicakee <stanfill11@gmail.com>
2024-04-22 15:28:03 -0600

    solved problem 3

--- aa03-additional-practice/problems/03-even-number-range.js
+++ aa03-additional-practice/problems/03-even-number-range.js
@@ -9,9 +9,20 @@
 */
 
 function evenNumberRange(num1, num2) {
-    // Your code here 
+    let newArray = [];
+    if(num1 > num2) return newArray;
+    
+    for(let i = num1; i <= num2; i++) {
+        if (i % 2 === 0) {
+            newArray.push(i);
+        }
+    }
+    return newArray;
 }
 
+console.log(evenNumberRange(-3, 2)); // prints [-2, 0, 2]
+console.log(evenNumberRange(22, 25)); // prints [22, 24]
+console.log(evenNumberRange(2, 0)); // prints []
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
     module.exports = evenNumberRange;
